3fcd10c93fa663957479d1f526cc6eefeaa29fba
hagguishel <hagguirzt@gmail.com>
2025-05-19 11:21:26 +0200

    ajout de la doc

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""Module square
+Ce module définit une classe vide appelée Square.
+Elle servira de base pour représenter un carré géométrique.
+"""
+
+
 class Square:
+    """Classe représentant un carré vide.
+
+    Cette classe ne contient pour l’instant aucun attribut ni méthode.
+    Elle est utilisée comme point de départ pour explorer la POO en Python.
+    """
+
     pass
