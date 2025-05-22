3fa20b92a8f11f3abf0d1efdb91817b769d2d3bb
hagguishel <hagguirzt@gmail.com>
2025-05-22 12:12:03 +0200

    exo 2

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,15 +1,40 @@
 #!/usr/bin/python3
-"""Classe Rectangle avec largeur et hauteur."""
+"""Classe Rectangle avec largeur et hauteur vérifiées."""
 
 
 class Rectangle:
     """Représente un rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initialise un rectangle.
-        Args:
-           width (int): largeur du rectangle.
-           height (int): hauteur du rectangle.
-        """
-        self.__width = width
-        self.__height = height
+        """Initialise un rectangle avec une largeur et une hauteur."""
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retourne la largeur du rectangle."""
+        return self.__width
+
+    width.setter
+
+    def width(self, value):
+        """Définit la largeur du rectangle avec vérifications."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    height.setter
+
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
