75180c4c6f51312e31ababe3b1ee615f3f40a6ab
hagguishel <hagguirzt@gmail.com>
2025-05-22 12:25:58 +0200

    pycode

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -18,7 +18,8 @@ class Rectangle:
         return self.width * self.height
 
     def perimeter(self):
-        """Retourne le périmètre du rectangle, ou 0 si width ou height vaut 0."""
+        """Retourne le périmètre du rectangle,
+        ou 0 si width ou height vaut 0."""
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
