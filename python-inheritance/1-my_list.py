8ce432c7e55a2c335f7910e228397dae2e8ac223
hagguishel <hagguirzt@gmail.com>
2025-05-27 12:47:17 +0200

    test

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-Module that defines the lookup function
+Module for MyList class.
+Defines MyList, a subclass of list with a method
+to display its elements in ascending order.
 """
 
 
 class MyList(list):
-    """Classe qui hérite de la classe list.
-
-    Cette classe ajoute une méthode pour afficher la liste triée
-    sans modifier l'ordre original de la liste.
-    """
+    """List subclass with a print_sorted method."""
 
     def print_sorted(self):
-        """Affiche la liste triée en ordre croissant."""
+        """
+        Display the list elements in ascending order.
+        """
         print(sorted(self))
