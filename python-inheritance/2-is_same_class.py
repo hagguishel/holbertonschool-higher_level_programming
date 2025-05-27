2c94309c10acba3ceb81fb032e9c899f679ae279
hagguishel <hagguirzt@gmail.com>
2025-05-27 12:43:12 +0200

    codestyle

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,4 +6,4 @@ Module that defines the lookup function
 
 def is_same_class(obj, a_class):
     """Vérifie si obj est une instance exacte de a_class."""
-    return type(obj) == a_class
+    return type(obj) is a_class
