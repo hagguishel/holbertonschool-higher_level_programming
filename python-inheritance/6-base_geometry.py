7169019b81b80fb502b6a5ec7f281e57e3819c4d
hagguishel <hagguirzt@gmail.com>
2025-05-27 13:17:57 +0200

    exo 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Définition d'une classe géométrique vide."""
+
+
+class BaseGeometry:
+    """Classe de base pour la géométrie."""
+
+    def area(self):
+        """Méthode non implémentée pour calculer l'aire."""
+        raise Exception("area() is not implemented")
