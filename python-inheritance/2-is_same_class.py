3b3bdba7f60be2ecf210784cd17d671c687991f0
hagguishel <hagguirzt@gmail.com>
2025-05-27 12:41:38 +0200

    exo 3

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Module that defines the lookup function
+"""
+
+
+def is_same_class(obj, a_class):
+    """Vérifie si obj est une instance exacte de a_class."""
+    return type(obj) == a_class
