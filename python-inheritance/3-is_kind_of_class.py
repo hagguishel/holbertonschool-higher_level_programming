3b0371ed7e628707cb341263d1d85f000cb1266a
hagguishel <hagguirzt@gmail.com>
2025-05-27 12:55:03 +0200

    test

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module that defines the lookup function
+"""
+
+
 def is_kind_of_class(obj, a_class):
     """Vérifie si obj est une instance ou une instance héritée de a_class."""
     return isinstance(obj, a_class)
