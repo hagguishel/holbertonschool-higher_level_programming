ddfc70e85ef0b94fb19d2938d26c7df441bd9c11
hagguishel <hagguirzt@gmail.com>
2025-06-02 16:17:23 +0200

    doc

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""
+Module 2-append_write
+
+Ce module contient une fonction qui ajoute une chaîne à la fin
+d’un fichier texte (UTF-8) et retourne le nombre de caractères ajoutés.
+"""
+
+
 def append_write(filename="", text=""):
+    """
+    Ajoute une chaîne à la fin d’un fichier texte (UTF-8).
+
+    Retourne :
+        Le nombre de caractères ajoutés.
+    """
     with open(filename, "a") as f:
         return f.write(text)
