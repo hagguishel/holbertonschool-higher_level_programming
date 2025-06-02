98c87c65196be7135fcbfedb96f6bb010758b60a
hagguishel <hagguirzt@gmail.com>
2025-06-02 15:36:42 +0200

    add doc

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    """
+    Lit un fichier texte (UTF-8) et affiche son contenu dans le terminal.
+
+    Argument :
+        filename : nom du fichier à lire.
+    """
     with open(filename, encoding="utf-8") as f:
         text = f.read()
 
