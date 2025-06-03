5b4398f8f50a531f5bd2e638a5184ed9f954abc9
hagguishel <hagguirzt@gmail.com>
2025-06-03 14:14:17 +0200

    correction

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""7. Load, add, save"""
+"""7. Load, add, save
 
-import sys  # Pour récupérer les arguments passés en ligne de commande
-import os  # Pour vérifier si le fichier existe
-from save_to_json_file import (
-    save_to_json_file,
-)  # Fonction pour sauvegarder une liste en JSON
-from load_from_json_file import (
-    load_from_json_file,
-)  # Fonction pour charger une liste depuis un fichier JSON
+Script qui ajoute tous les arguments passés à la ligne de commande
+à une liste JSON sauvegardée dans le fichier add_item.json.
+Utilise les fonctions save_to_json_file et load_from_json_file.
+"""
 
-filename = "add_item.json"  # Nom du fichier où la liste est stockée
+import sys  # Pour récupérer les arguments en ligne de commande
+from json.decoder import JSONDecodeError  # Pour gérer le cas où le fichier est vide
 
-# Vérifie si le fichier existe pour le charger, sinon initialise une liste vide
-if os.path.exists(filename):
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+filename = "add_item.json"
+
+# Essaye de charger la liste depuis le fichier, ou initialise une liste vide
+try:
     my_list = load_from_json_file(filename)
-else:
+except (FileNotFoundError, JSONDecodeError):
     my_list = []
 
-# Ajoute les arguments passés en ligne de commande à la liste
+# Ajoute les nouveaux arguments passés au script
 my_list.extend(sys.argv[1:])
 
 # Sauvegarde la liste mise à jour dans le fichier JSON
