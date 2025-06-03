aaeca9b4b41f2026d3cdb9aea1d5b303b96bcb9a
hagguishel <hagguirzt@gmail.com>
2025-06-03 13:20:11 +0200

    correction

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-"""5. Read file"""
-import sys
-import os
+"""7. Load, add, save"""
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+import sys  # Pour récupérer les arguments passés en ligne de commande
+import os  # Pour vérifier si le fichier existe
+from save_to_json_file import (
+    save_to_json_file,
+)  # Fonction pour sauvegarder une liste en JSON
+from load_from_json_file import (
+    load_from_json_file,
+)  # Fonction pour charger une liste depuis un fichier JSON
 
+filename = "add_item.json"  # Nom du fichier où la liste est stockée
 
-if os.path.exists("add_item.json"):
-    items = load_from_json_file("add_item.json")
+# Vérifie si le fichier existe pour le charger, sinon initialise une liste vide
+if os.path.exists(filename):
+    my_list = load_from_json_file(filename)
 else:
-    items = []
+    my_list = []
 
-items.extend(sys.argv[1:])
-save_to_json_file(items, "add_item.json")
+# Ajoute les arguments passés en ligne de commande à la liste
+my_list.extend(sys.argv[1:])
+
+# Sauvegarde la liste mise à jour dans le fichier JSON
+save_to_json_file(my_list, filename)
