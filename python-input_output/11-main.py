0ff2aabc9857e817faf1e19bdf1ef75b7709c5a3
hagguishel <hagguirzt@gmail.com>
2025-06-05 12:10:26 +0200

    correction

--- python-input_output/11-main.py
+++ python-input_output/11-main.py
@@ -1,50 +1,49 @@
 #!/usr/bin/python3
-import os
-import sys
-
-Student = __import__("11-student").Student
-read_file = __import__("0-read_file").read_file
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-path = sys.argv[1]
-
-if os.path.exists(path):
-    os.remove(path)
-
-student_1 = Student("John", "Doe", 23)
-j_student_1 = student_1.to_json()
-print("Initial student:")
-print(student_1)
-print(type(student_1))
-print(type(j_student_1))
-print("{} {} {}".format(student_1.first_name, student_1.last_name, student_1.age))
+"""11. Student to disk and reload"""
 
 
-save_to_json_file(j_student_1, path)
-read_file(path)
-print("\nSaved to disk")
+class Student:
+    """Class that defines a student."""
 
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a new Student instance.
 
-print("Fake student:")
-new_student_1 = Student("Fake", "Fake", 89)
-print(new_student_1)
-print(type(new_student_1))
-print(
-    "{} {} {}".format(
-        new_student_1.first_name, new_student_1.last_name, new_student_1.age
-    )
-)
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of the student.
 
-print("Load dictionary from file:")
-new_j_student_1 = load_from_json_file(path)
+        If attrs is a list of strings, only the specified attributes are returned.
+        Otherwise, all attributes are included.
 
-new_student_1.reload_from_json(j_student_1)
-print(new_student_1)
-print(type(new_student_1))
-print(
-    "{} {} {}".format(
-        new_student_1.first_name, new_student_1.last_name, new_student_1.age
-    )
-)
+        Args:
+            attrs (list or None): List of attribute names to include.
+        Returns:
+            dict: Dictionary of selected or all attributes.
+        """
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            my_dict = {}
+            for x in attrs:
+                if hasattr(self, x):
+                    my_dict[x] = getattr(self, x)
+            return my_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replace all attributes of the student instance with those from a dictionary.
+
+        Args:
+            json (dict): Dictionary containing attribute names and their values.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
