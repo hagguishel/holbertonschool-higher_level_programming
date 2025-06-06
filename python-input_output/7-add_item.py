39620f5af75456f512fe636c172167d8f7480555
hagguishel <hagguirzt@gmail.com>
2025-06-06 13:52:43 +0200

    correction du 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,23 +6,17 @@ Script qui ajoute tous les arguments passés à la ligne de commande
 Utilise les fonctions save_to_json_file et load_from_json_file.
 """
 
-import sys  # Pour récupérer les arguments en ligne de commande
-from json.decoder import JSONDecodeError  # Pour gérer le cas où le fichier est vide
+from sys import argv
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+conteneur = argv[1:]
 
-filename = "add_item.json"
-
-# Essaye de charger la liste depuis le fichier, ou initialise une liste vide
 try:
-    my_list = load_from_json_file(filename)
-except (FileNotFoundError, JSONDecodeError):
-    my_list = []
+    pelo = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    pelo = []
 
-# Ajoute les nouveaux arguments passés au script
-my_list.extend(sys.argv[1:])
-
-# Sauvegarde la liste mise à jour dans le fichier JSON
-save_to_json_file(my_list, filename)
+pelo.extend(conteneur)
+save_to_json_file(pelo, "add_item.json")
