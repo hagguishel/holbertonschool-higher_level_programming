355fb2fde1558b1f8288f50905cc7ba6f5e9e617
hagguishel <hagguirzt@gmail.com>
2025-06-24 12:14:37 +0200

    exo 3

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- 1 Root user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'; 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_1'@'localhost';
