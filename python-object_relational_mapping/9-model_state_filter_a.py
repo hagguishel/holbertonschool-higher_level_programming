efd50a937dd0a56283e1f38e35f8a4bb0fc4e2a7
hagguishel <hagguirzt@gmail.com>
2025-06-27 15:06:30 +0200

    correction

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,16 +13,18 @@ if __name__ == "__main__":
     sorted by id in ascending order.
     """
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
-        pool_pre_ping=True,
+        pool_pre_ping=True
     )
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).filter(State.name.like("%a%")).order_by(State.id):
+    for state in (session.query(State)
+                         .filter(State.name.like('%a%'))
+                         .order_by(State.id)):
         print("{}: {}".format(state.id, state.name))
 
     session.close()
