72e660c04b0ab57f313e82b782993f2012e033e9
hagguishel <hagguirzt@gmail.com>
2025-06-27 11:04:40 +0200

    correction

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
-    for row in rows:
-        print(f"{row[0]}: {row[1]}")
+    for row in cur.fetchall():
+        print(row)
     cur.close()
     conn.close()
