92095f41bba02e90d030d1075775dfabab6a9ddb
hagguishel <hagguirzt@gmail.com>
2025-06-27 15:04:02 +0200

    test

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-
 """
-Lists all State objects from the database hbtn_0e_6_usa
+Prints the first State object from the database hbtn_0e_6_usa
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 import sys
+from model_state import Base, State
 
 if __name__ == "__main__":
+    """
+    Connects to the database and lists all State objects,
+    sorted by id in ascending order.
+    """
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True,
     )
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = (
-        session.query(State).filter(State.name.like("%a%")).order_by(State.id).all()
-    )
-    for state in states:
-        print(f"{state.id}: {state.name}")
+    for state in session.query(State).filter(State.name.like("%a%")).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
 
     session.close()
