273a5831117a861e023fd379b4de40dffa7f66b6
hagguishel <hagguirzt@gmail.com>
2025-06-27 14:34:40 +0200

    correction

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""Start link class to table in database"""
-
-from sqlalchemy.ext.declarative import declarative_base
+"""
+Import classes that inherit from Base
+"""
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 
 class State(Base):
-    __tablename__ = "states"
+    """
+    Class for State
+    """
 
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
