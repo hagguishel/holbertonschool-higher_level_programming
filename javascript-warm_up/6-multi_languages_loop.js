e1eef9f7b573e2a6faca01726910d12e617d1019
hagguishel <hagguirzt@gmail.com>
2025-07-11 15:42:47 +0200

    correction

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
 const tableau = [
   'C is fun',
-  'Python is cool',
-  'JavaScript is amazing'
+  'Python is cool', 'JavaScript is amazing'
 ];
-
-let result = '';
 for (let i = 0; i < tableau.length; i++) {
-  result += tableau[i];
-if (i < tableau.length - 1) {
-  result += '\n';
-  }
+  console.log(tableau[i]);
 }
-console.log(result);
