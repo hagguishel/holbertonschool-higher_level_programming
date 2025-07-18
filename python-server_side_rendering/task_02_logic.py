9b5207a348912aa8607214374deafcfc1ff7bf5e
hagguishel <hagguirzt@gmail.com>
2025-07-18 11:40:38 +0200

    test

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -10,3 +10,6 @@ def items():
         item_list = data.get("items", [])
 
     return render_template("items.html", items=item_list)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
